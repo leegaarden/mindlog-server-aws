b97ddb9aa67aed79c360b06a2a09e1ba17f2fd7d
zwei1garden <dlwjddnjs081723@naver.com>
2024-05-14 01:42:37 +0900

    first commit: aws

---
+++ server/src/main/java/com/mindbridge/server/repository/MindlogRepository.java
@@ -0,0 +1,14 @@
+package com.mindbridge.server.repository;
+
+import com.mindbridge.server.model.Mindlog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface MindlogRepository extends JpaRepository<Mindlog, Long> {
+    @Query("SELECT m FROM Mindlog m WHERE m.date = :date")
+    List<Mindlog> findByDate(@Param("date") Date date);
+}
